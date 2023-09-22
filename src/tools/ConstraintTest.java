5cf0cb97b2e03779587051b9335391dc75ee6e5a
LeoZaid16 <leonardo-zaid@outlook.es>
2023-09-22 11:59:48 -0600

    Test case1 Constraint

---
+++ src/tools/ConstraintTest.java
@@ -0,0 +1,14 @@
+package tools;
+
+/**
+ *
+ * @author HP
+ */
+public class ConstraintTest {
+    public static void main(String ...args) {
+        Constraint c1 = new Constraint(-1,3,30);
+        System.out.println("P1:" + c1.getP1());
+        System.out.println("P2:" + c1.getP2());
+    }
+    
+}
